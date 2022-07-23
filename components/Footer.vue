a71be6be11c82a8869797a1f7d39fb105d3f70c5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-23 11:17:30 +0900

    Header, Footerの切り離し

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+    }
+  }
+}
+</script>
