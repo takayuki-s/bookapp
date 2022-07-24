fc3287c877c570e4bd6899160f198ff5e1cac913
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-25 08:02:54 +0900

    beforeRouteEnter（ナビゲーションガード）

--- pages/book/edit/_id.vue
+++ pages/book/edit/_id.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
-    book/edit/_id<br>
-    {{ $route.params.id }}
-    {{ books[$route.params.id].title }}
+    {{ book.title }}
   </div>
 </template>
 
 <script>
 export default {
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.book = vm.books[vm.$route.params.id]
+    })
+  },
   props: {
     books: {
       type: Array,
       default: () => {}
     }
-  }
+  },
+  data() {
+    return {
+      book: ''
+    }
+  },
 }
 </script>
