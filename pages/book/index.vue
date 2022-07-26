77a07f0f69b73e77851676cf4fbb13a2e0560389
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-26 09:25:01 +0900

    book一覧表示

--- pages/book/index.vue
+++ pages/book/index.vue
@@ -1,10 +1,50 @@
 <template>
   <div>
-    book/index
+    <v-row>
+      <v-col cols="6">
+        <v-btn color="primary" to="/book/search">検索する</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="book in books"
+        :key="book.id"
+        cols="12" md="6"
+        class="mb-4"
+      >
+        <v-card>
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image" />
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              読んだ日: {{ book.readDate }}<br>
+              感想: {{ book.memo }}
+              <v-spacer />
+              <v-card-actions>
+                <v-btn
+                  :to="{ name: 'book-edit-id', params: {id: book.id} }"
+                  color="info"
+                  fab small dark
+                ><v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    books: {
+      type: Array,
+      default: () => {}
+    }
+  },
 }
 </script>
