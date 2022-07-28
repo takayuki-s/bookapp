94b5b0acdf1c46ac481ae3ae50702a67082435de
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-29 08:47:08 +0900

    firestoreデータ取得

---
+++ pages/firebasetest/showData.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    データの表示<br />
+    <div v-for="(task, index) in tasks" :key="index">
+      id: {{ task.id }} / title: {{ task.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+export default {
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  async created() {
+    try {
+      const db = getFirestore(this.$firebase)
+      const querySnapshot = await getDocs(collection(db, 'tasks'))
+      querySnapshot.forEach((doc) => {
+        this.tasks.push(doc.data())
+      })
+    } catch(e) {
+      console.error('error: ', e)
+    }
+  },
+}
+</script>
