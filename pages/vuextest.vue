92cc9203375eb9e579e93006a3fc0b8a6737d111
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-08-01 08:08:43 +0900

    Vuexテスト（モジュール）

--- pages/vuextest.vue
+++ pages/vuextest.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
     <v-btn @click="$store.commit('increment')">+</v-btn>
-    {{ $store.state.counter }}
+    {{ $store.state.counter }}<br />
+    <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+    </li>
+    <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
+  </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapMutations } from 'vuex'
+export default {
+  computed: {
+    todos () {
+      return this.$store.state.todos.list
+    }
+  },
+  methods: {
+    addTodo (e) {
+      this.$store.commit('todos/add', e.target.value)
+      e.target.value = ''
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    })
+  }
+}
 </script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
